94ef522e6db6200e593be6747fc968a4955a20f1
DanielNgrd <113102346+DanielNgrd@users.noreply.github.com>
2022-09-15 16:24:07 +0200

    Update main.c
    Daiel commit test sram

--- main.c
+++ main.c
@@ -2,35 +2,100 @@
  * Lab_TTK4155.c
  *
  * Created: 01.09.2022 15:17:23
- * Modified: 14.09.2022 21:11
- * Author : Daniel
+ * Author : danie
  */ 
-
+#define F_CPU 4915200
+#define FOSC 4915200
+#define BAUD 9600 
+#define MYUBRR FOSC/16/BAUD-1 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define F_CPU 4915200
-#define FOSC 4915200
-#define BAUD 9600 
-#define MYUBRR FOSC/16/BAUD-1 
-#define BASE_ADDRESS 0x1000;
 
 
 void UART_init ( unsigned int ubrr );
-void UART_Transmitt (unsigned char data);
-unsigned char UART_Receive( void ); 
+void UART_Transmitt (unsigned char data, FILE * file);//, FILE * file
+unsigned char UART_Receive(FILE * file); // FILE * file 
 
-
+/*
 static FILE uart_stdio = FDEV_SETUP_STREAM(
 	(int(*)(char, struct __file *))UART_Transmitt,
 	(int(*)(struct __file *))UART_Receive,
 	_FDEV_SETUP_RW 
 	);
+*/
 
-void xmem_write ( uint8_t data , uint16_t addr );
-void xmem_init ( void );
+int main(void)
+{
+	
+	SRAM_init();
+	UART_init(MYUBRR);
+	//stdout = &uart_stdio;
+	//stdin = &uart_stdio;
+	//unsigned char recived_char;
+	SRAM_test();
+	
+    while (1) 
+    {
+		
+	
+		//printf("a");
+		//recived_char = UART_Receive(FILE * file);// FILE * file
+		//printf("%c",recived_char);
+	
+		/*if (recived_char == 's')
+		{
+			SRAM_test();
+		}
+		*/
+    }
+}
+
+void UART_init ( unsigned int ubrr ){
+	
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
+	
+	fdevopen(UART_Transmitt, UART_Receive);
+
+	
+}
+
+void UART_Transmitt (unsigned char data, FILE * file){//, FILE * file
+	
+	while (!(UCSR0A & (1<<UDRE0)))
+		;
+		UDR0 = data;
+
+	return 0;
+		
+	
+}
+
+unsigned char UART_Receive (FILE * file){//FILE * file
+	
+	while (!(UCSR0A &(1<<RXC0))){
+		
+	return UDR0;
+	}
+}
+
+//
+//void xmem_init ( void ){
+	//MCUCR |= (1 << SRE ); // enable XMEM
+	//SFIOR |= (1 << XMM0 ); // mask unused bits
+//}
+
+void SRAM_init() {
+	MCUCR |= (1 << SRE); 
+	SFIOR |= (1 << XMM2)|(1<<XMM0); //fjerner JTAG
+	
+}
 
 void SRAM_test(void)
 {
@@ -65,105 +130,5 @@ void SRAM_test(void)
 		}
 	}
 	printf("SRAM test completed with \n%4d errors in write phase and \n%4d errors in retrieval phase\n\n", write_errors, retrieval_errors);
-}
-	
-//void debug_println(const char * frmt, ...)
-//{
-	//char buf[255] = {0};
-	//va_list args;
-	//va_start(args, frmt);
-	//vsnprintf(buf,255,frmt,args);
-	//va_end(args);
-//
-//}
-
-//void str_write(const char *s)
-//{
-	//while(io_write(&USART_DEBUG.io, (const uint8_t *)s, strlen(s)) == ERR_NO_RESOURCE)
-	//{
-        //; //It's possible that you could call str_write faster than the UART driver can empty the FIFO
-        //// So wait for the current write to finish before allowing another to write
-        //// Keep in mind that this will make writing UART data eat more time.
-	//}
-//}
-
-
-int main(void)
-{
-	//DEBUG_println("t");
-	
-	//xmem_init();
-	UART_init(MYUBRR);
-	
-	UART_Receive();
-	char* buff[10];
-	for(int i = 0; i< 10; i++){
-		UART_Transmitt("H");	
-	}
-	
-	
-	stdout = &uart_stdio;
-	stdin = &uart_stdio;	
-	//SRAM_test();
-	
-unsigned char recived_char;
-
-    while (1) 
-    {
-		
-	;
-		
-
-		//printf(1);
-		//UART_Transmitt("h");
-		//UART_Receive();
-		
-			
-		//_delay_ms(1000);
-    }
-}
-
-void UART_init ( unsigned int ubrr ){
-	
-	UBRR0H = (unsigned char)(ubrr>>8);
-	UBRR0L = (unsigned char)ubrr;
-	
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
-
-	
-}
-
-
-
-void UART_Transmitt (unsigned char data){
-	
-	while (!(UCSR0A & (1<<UDRE0))){}
-		
-	UDR0 = data;
-		
-}
-
-
-
-unsigned char UART_Receive (void){
-	
-	while (!(UCSR0A &(1<<RXC0)))
-	
-		;
-		
-	return UDR0;
-	
-}
-
-void xmem_init ( void ){
-	MCUCR |= (1 << SRE ); // enable XMEM
-	SFIOR |= (1 << XMM2 ); // mask unused bits
-			
-			
-}
-void xmem_write ( uint8_t data , uint16_t addr ){
-	volatile char * ext_mem = ( char *) BASE_ADDRESS ;
-	ext_mem [ addr ]= data ;
 }
 
